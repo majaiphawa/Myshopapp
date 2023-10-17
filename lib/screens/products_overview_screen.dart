2f2561769947e2da888e5d4e6bb1d1c013cd4c43
majaiphawa <majaiphawa@gmail.com>
2023-10-17 19:57:03 +0530

    UI shop

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:my_shopapp/screens/cart_screen.dart';
+import '../providers/product_provider.dart';
+import '/widgets/app_drawer.dart';
+import 'package:my_shopapp/widgets/products_grid.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
+import '../providers/product.dart';
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductOverviewScreen extends StatefulWidget {
+  const ProductOverviewScreen({super.key});
+
+  @override
+  State<ProductOverviewScreen> createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  var _showOnlyFavorites = false;
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void iniState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetProducts().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("MyShop"), actions: <Widget>[
+        PopupMenuButton(
+          onSelected: (selectedvalue) {
+            setState(() {
+              if (selectedvalue == FilterOptions.Favorites) {
+                _showOnlyFavorites = true;
+              } else {
+                _showOnlyFavorites = false;
+              }
+            });
+          },
+          icon: Icon(
+            Icons.more_vert,
+          ),
+          itemBuilder: (BuildContext context) =>
+              <PopupMenuEntry<FilterOptions>>[
+            PopupMenuItem<FilterOptions>(
+              value: FilterOptions.Favorites,
+              child: Text("Only Favorites"),
+            ),
+            PopupMenuItem<FilterOptions>(
+              value: FilterOptions.All,
+              child: Text("Show All"),
+            ),
+          ],
+        ),
+        Consumer<Cart>(
+          builder: (context, cart, ch) {
+            return Badge(
+                label: Text(cart.itemCount.toString()),
+                child: IconButton(
+                  icon: Icon(Icons.shopping_cart),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(CartScreen.routeName);
+                  },
+                ));
+          },
+        ),
+      ]),
+      drawer: AppDrawer(),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ProductsGrid(_showOnlyFavorites),
+    );
+  }
+}
